36b10d502a7854a73296937dc84b1d4d864eecc0
LiLiLiLaLa <1921300809@qq.com>
2018-10-11 22:35:24 +0800

    第一次Java代码和笔记

---
+++ Test.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Test{
+    public static void main(String[] args){
+        System.out.println("邹二狗吃粑粑!");
+        //System.out.println(args[0]);
+    }
+}
